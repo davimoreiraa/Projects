83bcab27aee3e00dbba4fd9490696e5b09a0f998
unknown <davimoreiraasantoss@gmail.com>
2024-08-03 20:08:34 -0300

    insert the logo into the header and import the fonts

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,16 @@
 /* ------------------  STYLES -------------------*/
 import styles from './styles.module.css'
 
+/* ------------------  IMGS -------------------*/
+import assBck from '../../assets/images/assignature_bck.png'
+// import { assWht } from 
+
 export default function Header() {
     return (
-        <header className={`${styles.header}`}>
-            <div>
-                <nav className={``}>
+        <header className={`${styles.header} p-5`}>
+            <div className={`d-flex`}>
+                <img src={assBck} alt='logo' className={`${styles.logo}`}/>
+                <nav className={`flex-grow-1`}>
                     <ul className={`list-unstyled d-flex justify-content-around`}>
                         <li className={``}>Principais projetos</li>
                         <li>Sobre mim</li>
--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import 'bootstrap/dist/css/bootstrap.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
+   <head>
+     <link rel="preconnect" href="https://fonts.googleapis.com" />
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet" />
+   </head>
    <Home />
   </>
 );
