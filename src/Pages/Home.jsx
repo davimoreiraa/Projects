8508a6c648d4e8f6fa04aff1ae37fcc1f2b7172d
unknown <davimoreiraasantoss@gmail.com>
2024-08-04 19:58:58 -0300

    finished the toggle btn

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,14 @@
-/* ------------------  COMPONENTS -------------------*/
+/* ------------------ COMPONENTS -------------------*/
 import Footer from '../components/Header'
 import Header from '../components/Header'
 
-/* ------------------  STYLES -------------------*/
+/* ------------------ STYLES -------------------*/
 import styles from './styles.module.css'
 
 export default function Home() {
     return (
         <>
             <Header />
-            <label className={`${styles.switch}`}>
-                <input type="checkbox"  />
-                <span  className={`${styles.slider} ${styles.round} border border-black`}></span>
-            </label>
         </>
     )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,12 +8,16 @@ import assWht from '../../assets/images/assignature_wht.png'
 /* ------------------ RESOURCES -------------------*/
 import { useState } from 'react'
 
+/* ------------------ ICONS -------------------*/
+import { FaRegMoon } from "react-icons/fa6";
+import { FaRegSun } from "react-icons/fa6";
+
 
 export default function Header() {
     const [dark, setDark] = useState(true)
 
     return (
-        <header className={`${styles.header} p-5`}>
+        <header className={`${styles.header} ${dark ? '' : styles.light} p-5`}>
             <div className={`d-flex`}>
                 <img src={assBck} alt='logo' className={`${styles.logo}`}/>
                 <nav className={`flex-grow-1`}>
@@ -22,10 +26,12 @@ export default function Header() {
                         <li>Sobre mim</li>
                         <li>Principais ferramentas</li>
                     </ul>
-                    <label class="switch">
-                        <input type="checkbox" checked />
-                        <span class="slider round"></span>
+                    <FaRegSun  className={`me-1 ${dark ? '' : 'text-black'}`}/>
+                    <label className={`${styles.switch} ${dark ? 'border-light' : ' border-black'}`}>
+                        <input type="checkbox" checked={dark} onChange={e => setDark(e.target.checked)} />
+                        <span className={`${styles.slider} border ${styles.round} ${dark ? styles.dark : styles.light} ${dark ? 'border-light' : ' border-black'} ${dark ? 'bg-black' : 'bg-white'}`}></span>
                     </label>
+                    <FaRegMoon className={`ms-1 ${dark ? '' : 'text-black'}`} />
                 </nav>
             </div>
         </header>
