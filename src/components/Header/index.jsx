ded94ae6598bb40a2a7bb503eb3a5d8ca1062747
unknown <davimoreiraasantoss@gmail.com>
2024-08-04 22:10:37 -0300

    update header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -18,21 +18,25 @@ export default function Header() {
 
     return (
         <header className={`${styles.header} ${dark ? '' : styles.light} p-5`}>
-            <div className={`d-flex`}>
-                <img src={assBck} alt='logo' className={`${styles.logo}`}/>
-                <nav className={`flex-grow-1`}>
-                    <ul className={`list-unstyled d-flex justify-content-around`}>
+            <div className={`d-flex justify-content-between`}>
+                <div className={`col-1`}>
+                    <img src={dark ? assWht : assBck} alt='logo' className={`${styles.logo}`}/>
+                </div>
+                <nav className={`col-7 d-flex justify-content-between`}>
+                    <ul className={`${styles.list} list-unstyled d-flex justify-content-between`}>
                         <li className={``}>Principais projetos</li>
                         <li>Sobre mim</li>
                         <li>Principais ferramentas</li>
                     </ul>
+                </nav>
+                <div className={`col-2 d-flex justify-content-center`}>
                     <FaRegSun  className={`me-1 ${dark ? '' : 'text-black'}`}/>
-                    <label className={`${styles.switch} ${dark ? 'border-light' : ' border-black'}`}>
+                    <label className={`${styles.switch}`}>
                         <input type="checkbox" checked={dark} onChange={e => setDark(e.target.checked)} />
-                        <span className={`${styles.slider} border ${styles.round} ${dark ? styles.dark : styles.light} ${dark ? 'border-light' : ' border-black'} ${dark ? 'bg-black' : 'bg-white'}`}></span>
+                        <span className={`${styles.slider} border ${styles.round} ${dark ? 'border-light' : 'border-black'} ${dark ? styles.dark : styles.light}`}></span>
                     </label>
                     <FaRegMoon className={`ms-1 ${dark ? '' : 'text-black'}`} />
-                </nav>
+                </div>
             </div>
         </header>
     )
--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
-/* ------------------  RESOURCES -------------------*/
+/* ------------------ RESOURCES -------------------*/
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-/* ------------------  COMPONENTS -------------------*/
+/* ------------------ COMPONENTS -------------------*/
 import Home from './Pages/Home'
 
-/* ------------------  STYLES -------------------*/
+/* ------------------ STYLES -------------------*/
 import 'bootstrap/dist/css/bootstrap.css'
 import './styles/globals.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-   <head>
-     <link rel="preconnect" href="https://fonts.googleapis.com" />
-     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet" />
-   </head>
    <Home />
   </>
 );
