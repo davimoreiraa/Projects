d454a5aae98e2a697c582ddba21e51bc08cbddbb
unknown <davimoreiraasantoss@gmail.com>
2024-10-20 20:23:58 -0300

    update the colors at sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -16,16 +16,16 @@ import { AiFillTool } from "react-icons/ai";
 export default function Sidebar(props) {
 
     return (
-        <div className={`${styles.sidebar} ${props.isUp ? styles.up : ''} p-3 position-fixed top-0`}>
+        <div className={`${styles.sidebar} ${props.isUp ? styles.up : ''} ${props.dark ? '' : styles.light} p-3 position-fixed top-0`}>
             <div className={`d-flex justify-content-between mb-5`}>
                 <ThemeToggle dark={props.dark} changeTheme={props.changeTheme}/>
                 <div onClick={props.closeSideBar}><IoMdClose className={`fs-1 ${props.dark ? '' : 'text-black'}`}/></div>
             </div>
             <nav>
                 <ul className={`list-unstyled`}>
-                    <li className={`fs-3 mb-4 col-12`}><button className={`bg-transparent border-0 text-light`}><IoMdPerson /> Sobre mim</button></li>
-                    <li className={`fs-3 mb-4 col-12`}><button className={`bg-transparent border-0 text-light`}><RiPagesLine /> Principais projetos</button></li>
-                    <li className={`fs-3 mb-4 col-12`}><button className={`bg-transparent border-0 text-light`}><AiFillTool /> Principais ferramentas</button></li>
+                    <li className={`fs-3 mb-4 col-12`}><button className={`bg-transparent border-0 text-light ${props.dark ? '' : styles.light}`}><IoMdPerson /> Sobre mim</button></li>
+                    <li className={`fs-3 mb-4 col-12`}><button className={`bg-transparent border-0 text-light ${props.dark ? '' : styles.light}`}><RiPagesLine /> Principais projetos</button></li>
+                    <li className={`fs-3 mb-4 col-12`}><button className={`bg-transparent border-0 text-light ${props.dark ? '' : styles.light}`}><AiFillTool /> Principais ferramentas</button></li>
                 </ul>
             </nav>
         </div>
