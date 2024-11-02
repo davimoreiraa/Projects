764b393de3ea7937bbb04db0dd0ae75d36dcd18c
unknown <davimoreiraasantoss@gmail.com>
2024-11-02 14:53:05 -0300

    dark theme is now in home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,10 +5,25 @@ import Header from '../components/Header'
 /* ------------------ STYLES -------------------*/
 import styles from './styles.module.css'
 
+/* ------------------ RESOURCES -------------------*/
+import { useState } from 'react'
+
 export default function Home() {
+    const [dark, setDark] = useState(true)
+
+    const changeTheme = () => {
+        setDark(!dark)
+    }
+
     return (
         <div className={`${styles.container}`}>
-            <Header />
+            <Header dark={dark} changeTheme={changeTheme}/>
+            <main className={`${styles.content} ${dark ? styles.dark : styles.light}`}>
+                <section className={`${styles.projects}`}>
+                    <h1>Principais projetos</h1>
+                </section>
+            </main>
+            
         </div>
     )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -15,26 +15,21 @@ import ThemeToggle from '../ThemeToggle'
 /* ------------------ ICONS -------------------*/
 import { CiMenuBurger } from "react-icons/ci";
 
-export default function Header() {
-    const [dark, setDark] = useState(true)
+export default function Header(props) {
     const [sidebar, setSidebar] = useState(false)
 
     const closeSideBar = () => {
         setSidebar(false)
     }
 
-    const changeTheme = () => {
-        setDark(!dark)
-    }
-
     return (
-        <header className={`${styles.header} ${dark ? '' : styles.light} p-5`}>
+        <header className={`${styles.header} ${props.dark ? '' : styles.light} p-5`}>
             <div className={`d-flex justify-content-between`}>
                 <div className={`col-4 col-sm-2 col-md-1`}>
-                    <img src={dark ? assWht : assBck} alt='logo' className={`${styles.wd}`}/>
+                    <img src={props.dark ? assWht : assBck} alt='logo' className={`${styles.wd}`}/>
                 </div>
                 <div className={`d-flex col-9 col-xl-8 justify-content-around`}>
-                    <nav className={`${dark ? styles.dark : styles.light} col-7 d-flex justify-content-between`}>
+                    <nav className={`${props.dark ? styles.dark : styles.light} col-7 d-flex justify-content-between`}>
                         <ul className={`${styles.wd} d-none d-lg-flex m-0 list-unstyled justify-content-between`}>
                             <li className={`d-flex align-items-center`}><a href='google.com' className={`${styles.nav_link}`}>Principais projetos</a></li>
                             <li className={`d-flex align-items-center`}><a href='google.com' className={`${styles.nav_link}`}>Sobre mim</a></li>
@@ -42,15 +37,15 @@ export default function Header() {
                         </ul>
                     </nav>
                     <div className={`d-none d-lg-flex col-2 justify-content-center align-items-center`}>
-                        <ThemeToggle dark={dark} changeTheme={changeTheme}/>
+                        <ThemeToggle dark={props.dark} changeTheme={props.changeTheme}/>
                     </div>
                     <div className={`${styles.dark} fs-1 d-flex d-lg-none align-items-center`}>
-                       <button className={`${dark ? styles.dark : styles.light} ${styles.btn}`} onClick={() => setSidebar(!sidebar)}><CiMenuBurger /></button>  
+                       <button className={`${props.dark ? styles.dark : styles.light} ${styles.btn}`} onClick={() => setSidebar(!sidebar)}><CiMenuBurger /></button>  
                     </div>
                 </div>
             </div>
             <div className={`${styles.sidebar_container}`}>
-                <Sidebar dark={dark} changeTheme={changeTheme} closeSideBar={closeSideBar} isUp={sidebar}/>
+                <Sidebar dark={props.dark} changeTheme={props.changeTheme} closeSideBar={closeSideBar} isUp={sidebar}/>
             </div>
         </header>
     )
